97679d4d6d8f8589f8aa4ff7a9d1145ab98b7b4c
Lance Jordan <djlancejordan@gmail.com>
2017-06-17 14:00:53 -0400

    dont persist guest users in db

--- lib/devise-guests/controllers/helpers.rb
+++ lib/devise-guests/controllers/helpers.rb
@@ -67,7 +67,7 @@ module DeviseGuests::Controllers
             g.send("\#{auth_key}=", send(:"guest_\#{auth_key}_authentication_key", key))
             g.guest = true if g.respond_to? :guest
             g.skip_confirmation! if g.respond_to?(:skip_confirmation!)
-            g.save(validate: false)
+            #g.save(validate: false)
           end
         end
 
